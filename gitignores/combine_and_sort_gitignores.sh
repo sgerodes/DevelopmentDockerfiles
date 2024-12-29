1313c6f3c797c8a48d564468f156ff6f32f8164a
sgerodes <sgerodes@gmail.com>
2024-12-29 18:18:41 +0100

    feat: adds sqlite ignores

--- gitignores/combine_and_sort_gitignores.sh
+++ gitignores/combine_and_sort_gitignores.sh
@@ -2,7 +2,7 @@
 
 # Combine all .gitignore files, remove comments and blank lines, and sort
 #cat .gitignore_os .gitignore_python .gitignore_rust | \
-cat custom.gitignore jetbrains.gitignore macos.gitignore node.gitignore python.gitignore rust.gitignore | \
+cat custom.gitignore jetbrains.gitignore macos.gitignore node.gitignore python.gitignore rust.gitignore sqlite.gitignore | \
 grep -v '^\s*#' | grep -v '^\s*$' | sort -u > .gitignore
 
 echo "Combined and sorted entries from all .gitignore files into .gitignore."
